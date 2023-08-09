2802906e9c7df16eb535140b2b98664ebeb94b5d
Anna D <hello.soandon@gmail.com>
2023-08-09 18:04:26 +0200

    Remove unused

--- src/store/apiSlice.ts
+++ src/store/apiSlice.ts
@@ -1,5 +1,3 @@
-/* React-specific entry point that automatically generates
-   hooks corresponding to the defined endpoints */
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
